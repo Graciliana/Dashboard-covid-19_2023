dddf6353639301aad90534c4110bcba84bf9ec0d
user.email like : "eaGraciliana@Gmail.com <user.email like : "eaGraciliana@Gmail.com>
2023-05-25 13:13:17 -0300

    :construction: feat: Limpeza dos datasets e criacao dos Dataframes auxiliares

--- dashboard.py
+++ dashboard.py
@@ -10,3 +10,20 @@ import plotly.graph_objects as go
 import numpy as np
 import pandas as pd
 import json
+
+#leitura dos dados Covid2023
+df = pd.read_csv('covidbr2023.csv', sep=";")
+df_states_city =  df[(~df['estado'].isna())]
+#df_states_city
+# para remover os dados dos municipios 
+df_states =  df[(~df['estado'].isna()) & (df['codmun'].isna())]
+#dataframe dos estados e municipios
+#df_states
+#dataframe das regiao do Brasil 
+df_brasil =  df[df['regiao'] == 'Brasil']
+#df_brasil
+#exportar os dados para um arquivo csv
+df_states_city.to_csv('df_states_city.csv')
+df_states.to_csv('df_states.csv')
+df_brasil.to_csv('df_brasil.csv')
+
